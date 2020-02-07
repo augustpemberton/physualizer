28122d0885b306a5c11d537b2c9cee8c1fabf980
august <augustmpemberton@gmail.com>
2020-02-07 00:56:44 +0000

    Bouncing

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,15 +14,16 @@ void drawFrame();
 int8_t enc_delta(void);
 volatile int8_t delta;
 
-int running = 1;
-
 #define TIMESCALE_STEP 0.2
 float timescale = 1;
 
-#define STEP_DELAY 7
+#define NUM_PARTICLES 6
+
+#define STEP_DELAY 0
 #define BALL_RADIUS 10
-#define NUM_PARTICLES 5
 #define GRAVITY 9.8
+#define GROUNDED_THRESHOLD 0.4
+#define GROUNDED_EXIT_THRESHOLD 0.5
 
 #define MIN_X 0
 #define MIN_Y 0
@@ -41,6 +43,9 @@ typedef struct {
   Vector2 velocity;
   float radius;
   float mass;
+  float restitution;
+  float friction;
+  bool grounded;
 } Particle;
 
 Particle particles[NUM_PARTICLES];
@@ -48,19 +53,6 @@ Particle particles[NUM_PARTICLES];
 int randRange(int min, int max) {
   return (rand() % (max + 1 - min) + min);
 }
-
-float boundX(float n, float p) {
-  if (n - p < MIN_X) return MIN_X + p;
-  if (n + p > MAX_X) return MAX_X - p;
-  return n;
-}
-
-float boundY(float n, float p) {
-  if (n - p < MIN_Y) return MIN_Y + p;
-  if (n + p > MAX_Y) return MAX_Y - p;
-  return n;
-}
-
 
 void drawBounds() {
   SDL_RenderDrawLine(renderer, MIN_X, MIN_Y, MAX_X, MIN_Y);
@@ -70,9 +62,13 @@ void drawBounds() {
 }
 
 void drawParticles() {
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
   for (int i=0; i < NUM_PARTICLES; ++i) {
     Particle *particle = &particles[i];
+    if (particle->grounded) {
+      SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    } else {
+      SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    }
     drawCircle(renderer, particle->position.x, particle->position.y, particle->radius);
   }
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -83,12 +79,17 @@ void clearParticles() {
 }
 
 void initializeParticles() {
+  float scale;
   for (int i=0; i<NUM_PARTICLES; ++i) {
-    particles[i].radius   = BALL_RADIUS;
-    particles[i].position = (Vector2){randRange( MIN_X + particles[i].radius, MAX_X - particles[i].radius), 
-                                      randRange( MIN_Y + particles[i].radius, MAX_Y - particles[i].radius)};
-    particles[i].velocity = (Vector2){40, 30};
-    particles[i].mass     = 1;
+    scale = 1;
+    particles[i].radius   = BALL_RADIUS * scale;
+
+    particles[i].position = (Vector2){MIN_X + ((MAX_X-MIN_X) / NUM_PARTICLES) * i,
+                                      MIN_Y + ((MAX_Y-MIN_Y) / NUM_PARTICLES) * i};
+    particles[i].velocity = (Vector2){randRange(-50, 50), randRange(-50, 50)};
+
+    particles[i].mass     = scale;
+    particles[i].grounded = false;
   }
 }
 
@@ -106,36 +107,86 @@ void applyContainerForce(Particle *particle) {
   if (particle->position.x - particle->radius < MIN_X) {
     particle->velocity.x = abs(particle->velocity.x) * COEFF_REST;
     particle->velocity.y *= COEFF_FRIC;
-    //particle->position.x += 2 * (MIN_X - (particle->position.x - particle->radius));
+
+    particle->position.x += particle->radius - (particle->position.x - MIN_X );
   }
   if (particle->position.x + particle->radius > MAX_X) {
     particle->velocity.x = -1 * abs(particle->velocity.x) * COEFF_REST;
     particle->velocity.y *= COEFF_FRIC;
-    //particle->position.x -= 2 * ((particle->position.x + particle->radius) - MAX_X);
+
+    particle->position.x -= particle->radius - (MAX_X - particle->position.x);
   }
   if (particle->position.y - particle->radius < MIN_Y) {
     particle->velocity.y = abs(particle->velocity.y) * COEFF_REST;
     particle->velocity.x *= COEFF_FRIC;
-    //particle->position.y += 2 * (MIN_Y - (particle->position.y - particle->radius));
+
+    particle->position.y += particle->radius - (particle->position.y - MIN_Y );
   }
   if (particle->position.y + particle->radius > MAX_Y) {
     particle->velocity.y = -1 * abs(particle->velocity.y) * COEFF_REST;
     particle->velocity.x *= COEFF_FRIC;
-    //particle->position.y -= 2 * ((particle->position.y + particle->radius) - MAX_Y);
+
+    particle->position.y -= particle->radius - (MAX_Y - particle->position.y);
+
+    if (abs(particle->velocity.y) < GROUNDED_THRESHOLD && abs(particle->velocity.x) < GROUNDED_THRESHOLD) {
+      particle->grounded = true;
+    }
   }
 }
 
-/*void applyCollisions(Particle *particle) {
+void applyCollisions() {
   // iterate through each particle
   // check its not our particle
   // apply collision forces
+  float distance;
+  for (int j=0; j<NUM_PARTICLES; ++j) {
+    Particle *particle = &particles[j];
+    for (int i=j+1; i<NUM_PARTICLES; ++i) {
+      Particle *particle2 = &particles[i];
+      distance = sqrt(pow(particle->position.x - particle2->position.x, 2) + 
+                      pow(particle->position.y - particle2->position.y, 2));
+      if (distance < (particle->radius + particle2->radius)) {
+        float dx = particle->position.x - particle2->position.x;
+        float dy = particle->position.y - particle2->position.y;
 
-  for (int i=0; i<NUM_PARTICLES; ++i) {
-    if (*particles[i]) {
+        float distanceToMove = (particle->radius + particle2->radius) - sqrt(pow(dx,2) + pow(dy,2));
 
+        float angle = atan(dy/dx);
+
+        float dtx = distanceToMove * cos(angle);
+        float dty = distanceToMove * sin(angle);
+
+        particle->position = (Vector2){particle->position.x-abs(dtx), particle->position.y-abs(dty)};
+        particle2->position = (Vector2){particle2->position.x+abs(dtx), particle2->position.y+abs(dty)};
+
+        // A.v = (A.u * (A.m - B.m) + (2 * B.m * B.u)) / (A.m + B.m)
+        // B.v = (B.u * (B.m - A.m) + (2 * A.m * A.u)) / (A.m + B.m)
+
+        Vector2 au = particle->velocity;
+        Vector2 bu = particle2->velocity;
+        float k = particle->mass + particle2->mass;
+
+        particle->velocity.x = (au.x * (particle->mass - particle2->mass) + (2 * particle2->mass * bu.x)) / k;
+        particle->velocity.y = (au.y * (particle->mass - particle2->mass) + (2 * particle2->mass * bu.y)) / k;
+
+        particle2->velocity.x = (bu.x * (particle2->mass - particle->mass) + (2 * particle->mass * au.x)) / k;
+        particle2->velocity.y = (bu.y * (particle2->mass - particle->mass) + (2 * particle->mass * au.y)) / k;
+
+        // check for grounded state
+        if (particle->position.y < particle2->position.y) {
+          if (abs(particle->velocity.y) < GROUNDED_THRESHOLD && abs(particle->velocity.x) < GROUNDED_THRESHOLD) {
+            particle->grounded = true;
+          }
+        } else {
+          if (abs(particle2->velocity.y) < GROUNDED_THRESHOLD && abs(particle2->velocity.x) < GROUNDED_THRESHOLD) {
+            particle2->grounded = true;
+          }
+        }
+
+      }
     }
   }
-}*/
+}
 
 clock_t previousTime;
 clock_t currentTime;
@@ -153,29 +204,33 @@ double getDT() {
 int main(void) {
     init();
     float dt = 0;
-    char buf[100];
     while (userQuit == 0) {
       drawBounds();
       handleEvents();
       dt = (getDT() / 100000) * timescale;
-      gcvt(dt, 6, buf);
-      printf("buffer: %s\n", buf);
 
       clearParticles();
+      applyCollisions();
       for (int i=0; i<NUM_PARTICLES; ++i) {
         Particle *particle = &particles[i];
+
+        if (particle->grounded == true && abs(particle->velocity.y) > GROUNDED_EXIT_THRESHOLD) {
+          particle->grounded = false;
+        }
+
+        if (particle->grounded) {
+          particle->velocity.y = abs(particle->velocity.y);
+        }
+
+        if (particle->grounded == false) {
+          applyForce(particle, gravityForce(particle), dt);
+        }
+
         applyContainerForce(particle);
-        applyForce(particle, gravityForce(particle), dt);
-
-        /*if ((rand()/(double)RAND_MAX) > 0.999) {
-          applyForce(particle, (Vector2){randRange(-50, 50), -5000}, dt);
-        }*/
-
-        //applyCollisions(particle);
-
         particle->position.x += particle->velocity.x * dt;
         particle->position.y += particle->velocity.y * dt;
       }
+
       drawParticles();
 
       SDL_RenderPresent(renderer);
@@ -187,8 +242,9 @@ int main(void) {
 }
 
 void init(void) {
-    initializeParticles();
-    initWindow();
-    previousTime = clock();
-    currentTime = clock();
+  initWindow();
+  previousTime = clock();
+  currentTime = clock();
+  //srand(time(NULL));
+  initializeParticles();
 }
--- src/graphics.c
+++ src/graphics.c
@@ -75,3 +75,4 @@ void drawCircle(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32
       }
    }
 }
+
